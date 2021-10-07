139ac84f19dc5b54e88d78d3aac8e46ca641919c
Divider <it-cio@mail.ru>
2021-10-07 09:29:31 +0300

    added global variables

--- ami.py
+++ ami.py
@@ -4,15 +4,21 @@ import os
 from dotenv import load_dotenv
 from panoramisk import Manager, Message
 
-load_dotenv()
-manager = Manager(
-    host=os.getenv('AMI_HOST'),
-    port=os.getenv('AMI_PORT'),
-    username=os.getenv('AMI_USERNAME'),
-    secret=os.getenv('AMI_SECRET'),
-    ping_delay=10,  # Delay after start
-    ping_interval=10,  # Periodically ping AMI (dead or alive)
-    reconnect_timeout=2)  # Timeout reconnect if connection lost
+
+try:
+    load_dotenv()
+    manager = Manager(
+        host=os.getenv('AMI_HOST'),
+        port=os.getenv('AMI_PORT'),
+        username=os.getenv('AMI_USERNAME'),
+        secret=os.getenv('AMI_SECRET'),
+        ping_delay=10,  # Delay after start
+        ping_interval=10,  # Periodically ping AMI (dead or alive)
+        reconnect_timeout=2)  # Timeout reconnect if connection lost
+except Exception as ex:
+    print(f'AMI-interface is not configured, or there is no connection. Error: {ex}')
+finally:
+    event, caller, number, status = [], [], [], []
 
 
 def on_connect(mngr: Manager):
@@ -33,7 +39,7 @@ def on_disconnect(mngr: Manager, exc: Exception):
 
 async def on_startup(mngr: Manager):
     await asyncio.sleep(0.1)
-    logging.info('Something action...')
+    logging.info('AMI-interface started')
 
 
 async def on_shutdown(mngr: Manager):
@@ -45,31 +51,29 @@ async def on_shutdown(mngr: Manager):
 @manager.register_event('*')  # Register all events
 async def ami_callback(mngr: Manager, msg: Message):
     if msg.Event == 'FullyBooted':
-        # print(msg)
-        return msg
+        event.append('AMI-interface started')
 
 
 @manager.register_event('Newchannel')
 async def callback(mngr: Manager, msg: Message):
     if msg.ChannelStateDesc == 'Down' and msg.Context != 'from-internal':
         if msg.CallerIDNum and msg.Exten:
-            caller = msg.CallerIDNum
-            number = msg.Exten
-            # print(f'\nIncoming call\nfrom number: {caller}\nto number: {number}')
-            return caller, number
+            caller.append(msg.CallerIDNum)
+            number.append(msg.Exten)
+            logging.info(f'Incoming call\nfrom number: {msg.CallerIDNum}\nto number: {msg.Exten}')
     await asyncio.sleep(1)
 
 
 @manager.register_event('Dial')
 async def callback(mngr: Manager, msg: Message):
     if msg.DialStatus == 'ANSWER':
-        # print('Call ended')
-        call = 'Call ended'
-        return call
+        status.append('end')
+        logging.info('Call ended')
     await asyncio.sleep(1)
 
 
-def ami_connect(state=True):
+def connect(state=True):
+    # logging.basicConfig(level=logging.INFO)
     manager.on_connect = on_connect
     manager.on_login = on_login
     manager.on_disconnect = on_disconnect
